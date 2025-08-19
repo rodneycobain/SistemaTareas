beca3fb948e2d5ed15deae508be9cdf13edeaa9d
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-19 11:42:18 +0200

    Twelveth Commit Seleccionar Filas tabla tarea

--- src/main/java/gm/tareas/controlador/IndexControlador.java
+++ src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -55,6 +55,8 @@ public class IndexControlador implements Initializable {
     @FXML
     private TextField estatusTexto;
 
+    private Integer idTareaInterno;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -68,7 +70,6 @@ public class IndexControlador implements Initializable {
         nombreTareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
         responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
         estatusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
-        
 
     }
 
@@ -91,6 +92,16 @@ public class IndexControlador implements Initializable {
             mostrarMensaje("Informacion", "Tarea agregada");
             limpiarFormulario();
             listarTareas();
+        }
+    }
+
+    public void cargarTareaFormulario(){
+        var tarea = tareaTabla.getSelectionModel().getSelectedItem();
+        if (tarea != null){
+            idTareaInterno =  tarea.getIdTarea();
+            nombreTareaTexto.setText(tarea.getNombreTarea());
+            responsableTexto.setText(tarea.getResponsable());
+            estatusTexto.setText(tarea.getEstatus());
         }
     }
 
